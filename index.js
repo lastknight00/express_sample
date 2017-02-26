20957b57a496ba3747d515ba5d818ff8c60f1c44
taehan Lee <darkknight8608@gmail.com>
2017-02-26 13:56:35 +0900

    Partner List API 추가

--- index.js
+++ index.js
@@ -10,140 +10,79 @@ app.use(bodyParser.text());
 app.use(bodyParser.json());
 
 app.get('/api/selectCountryInfo', function(req, res) {
-
-  //var country = req.query.country;
-console.log(req.query)
   var request = req.query;
-  console.log(request.IATA);
   if(request.IATA == 'true') {
-    var retval = {
-      "resultCode" : "S001"
-      ,"resultData" : {
-           "data" : [
-             {
-                             "countryInfo" : {
-                                  "name" : "South Korea"
-                                  ,"code" :  "KOR"
-                              }
-                            ,"IATAInfo" : [
-                                   { "name" :  "Incheon International", "IATA":"ICN", "city" : "Seoul", "country" : "south korea"}
-                                  ,{ "name" :  "Gimpo International", "IATA":"GMP", "city" : "Seoul2", "country" : "south korea"}
-                                  ,{ "name" :  "Gimhae International", "IATA":"PUS", "city" : "Busan", "country" : "south korea"}
-                                  ,{ "name" :  "Jeju International", "IATA":"CJU", "city" : "Jeju city", "country" : "south korea"}
-                           ]
-                           ,"picture" : [
-                                  { "contents" :"XXXXXXXXXXXXXXXXx", "format":"bmp", "wsize":"300", "hsize":"300"}
-                                 ,{ "contents" :"XXXXXXXXXXXXXXXXx", "format":"bmp", "wsize":"300", "hsize":"300"}
-                                 ,{ "contents" :"XXXXXXXXXXXXXXXXx", "format":"bmp", "wsize":"300", "hsize":"300"}
-                                 ,{ "contents" :"XXXXXXXXXXXXXXXXx", "format":"bmp", "wsize":"300", "hsize":"300"}
-                          ]
+    var retval = makeResultData([
+      {
+                      "countryInfo" : {
+                           "name" : "South Korea"
+                           ,"code" :  "KOR"
                        }
-                     ]
-           }
-           ,"tokenId" : "XXXXXXXXXXXXXXXX"
-           ,"tokenValidStartTime" : "YYYYMMDDhhmmsss"  //GMT 0 기준
-           ,"tokenValidEndTime" : "YYYYMMDDhhmmsss"  //GMT 0 기준
-    };
+                     ,"IATAInfo" : [
+                            { "name" :  "Incheon International", "IATA":"ICN", "city" : "Seoul", "country" : "south korea"}
+                           ,{ "name" :  "Gimpo International", "IATA":"GMP", "city" : "Seoul2", "country" : "south korea"}
+                           ,{ "name" :  "Gimhae International", "IATA":"PUS", "city" : "Busan", "country" : "south korea"}
+                           ,{ "name" :  "Jeju International", "IATA":"CJU", "city" : "Jeju city", "country" : "south korea"}
+                    ]
+                    ,"picture" : [
+                           { "contents" :"XXXXXXXXXXXXXXXXx", "format":"bmp", "wsize":"300", "hsize":"300"}
+                          ,{ "contents" :"XXXXXXXXXXXXXXXXx", "format":"bmp", "wsize":"300", "hsize":"300"}
+                          ,{ "contents" :"XXXXXXXXXXXXXXXXx", "format":"bmp", "wsize":"300", "hsize":"300"}
+                          ,{ "contents" :"XXXXXXXXXXXXXXXXx", "format":"bmp", "wsize":"300", "hsize":"300"}
+                   ]
+                }
+              ]);
   } else {
-    var retval = {
-       "resultCode" : "S001"
-       ,"resultData" : {
-            "data" : [
-              {
-                  "countryInfo" : {
-                   "name" : "Japan",
-                   "code" :  "JPN"
-                  }
-                  ,"picture" : [
-                   { "url":"http://cfile8.uf.tistory.com/image/255DC34B54C700871C698E",
-                     "contents" :"XXXXXXXXXXXXXXXXx", "format":"bmp", "wsize":"300", "hsize":"300"},
-                  ]
-                  }
-                  ,{
-                  "countryInfo" : {
-                   "name" : "South Korea",
-                   "code" :  "KOR"
-                  }
-                  ,"picture" : [
-                   { "url":"http://cfile24.uf.tistory.com/original/1410BD344EF1FEAD292FC3",
-                     "contents" :"XXXXXXXXXXXXXXXXx", "format":"bmp", "wsize":"300", "hsize":"300"},
-                  ]
-                  }
-                      ]
-            }
-            ,"tokenId" : "XXXXXXXXXXXXXXXX"
-            ,"tokenValidStartTime" : "YYYYMMDDhhmmsss"  //GMT 0 기준
-            ,"tokenValidEndTime" : "YYYYMMDDhhmmsss"  //GMT 0 기준
-       };
-
+    var retval = makeResultData([
+      {
+        "countryInfo" : {
+          "name" : "Japan",
+          "code" :  "JPN"
+        }
+        ,"picture" : [
+           { "url":"http://cfile8.uf.tistory.com/image/255DC34B54C700871C698E",
+             "contents" :"XXXXXXXXXXXXXXXXx", "format":"bmp", "wsize":"300", "hsize":"300"},
+          ]
+          }
+          ,{
+          "countryInfo" : {
+           "name" : "South Korea",
+           "code" :  "KOR"
+          }
+          ,"picture" : [
+           { "url":"http://cfile24.uf.tistory.com/original/1410BD344EF1FEAD292FC3",
+             "contents" :"XXXXXXXXXXXXXXXXx", "format":"bmp", "wsize":"300", "hsize":"300"},
+          ]
+          }
+      ]);
   }
   res.send(retval);
 });
 
-app.get('/getCityInfo', function(req, res) {
-  addApiDescription(retval, "This is API for search Country or City information. parameter List : [cityId]");//TODO to be deleted
-});
+app.get('/api/selectPartnerList', function(req, res) {
+  var retval = makeResultData(
+    [
+           {"partnerId":"com1", "partnerNm":"LTH Company" , "1bagBasicPrice":30, "currency":"EUR", "grade":3 }
+          ,{"partnerId":"comp2", "partnerNm":"PSI Company", "1bagBasicPrice":20, "currency":"EUR", "grade":5 }
+          ,{"partnerId":"comp3", "partnerNm":"MTI Company", "1bagBasicPrice":45, "currency":"EUR", "grade":4 }
+          ,{"partnerId":"comp4", "partnerNm":"HSR Company", "1bagBasicPrice":45, "currency":"EUR", "grade":4 }
+      ]
+  );
 
-app.get('/getPartnerList', function(req, res) {
-  addApiDescription(retval, "This is API for search Country or City information. parameter List : [partnerId]");//TODO to be deleted
-});
-
-app.get('/getPartnerPricePolicyInfo', function(req, res) {
-  addApiDescription(retval, "This is API for search Country or City information. parameter List : [partnerId]");//TODO to be deleted
-})
-
-app.post('/getBookList',function(req,res) {
-  var retval = makeResponseTemplate('01', {
-    resultMessage : 'Success'
-  }, datas.books);
-  addApiDescription(retval, "This is API for search book list. parameter List : [empty]");//TODO to be deleted
   res.send(retval);
 });
 
-app.get('/getBookInfo', function(req, res) {
-  var retval = makeResponseTemplate('01', {
-    resultMessage : 'Success'
-  }, datas.books);
-  addApiDescription(retval, "This is API for search book information. parameter List : [bookId]");//TODO to be deleted
-  res.send(retval);
-});
+var makeResultData = function(data) {
+  var retval = {
+     "resultCode" : "S001"
+     ,"resultData" : {
+          "data" : data
+      }
+      ,"tokenId" : "XXXXXXXXXXXXXXXX"
+      ,"tokenValidStartTime" : "YYYYMMDDhhmmsss"  //GMT 0 기준
+      ,"tokenValidEndTime" : "YYYYMMDDhhmmsss"  //GMT 0 기준
+   };
 
-app.post('/insertBookInfo', function(req, res) {
-  addApiDescription(retval, "This is API for search Country or City information. parameter List : [empty | countryId]");//TODO to be deleted
-});
-
-app.post('/deleteBookInfo', function(req, res) {
-
-});
-
-app.post('/insertPayment', function(req, res) {
-
-});
-
-app.get('/getLuggageLocationInfo', function(req, res) {
-
-});
-
-app.get('/getUsers', function(req, res) {
-  res.send(datas.users);
-});
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.listen(8080);
-
-var makeResponseTemplate = function(resultCode, resultInfo, resultData) {
-  return {
-    resultCode : resultCode,
-    resultInfo : resultInfo,
-    resultData : resultData
-  };
-};
-
-var addApiDescription = function(result, desc) {
-  result.description = desc;
+   return retval;
 }
+app.listen(8080);
